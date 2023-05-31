ced8a54e96cec5ef1179aa69da5b068344a4afe5
y9Kap <wotblitz6allima4ok@gmail.com>
2023-05-31 03:41:27 +0300

    ready to upload

--- src/main/kotlin/types/Date.kt
+++ src/main/kotlin/types/Date.kt
@@ -4,7 +4,7 @@ class Date(val dateString: String) {
     init {
         val regex = """\d{2}\.\d{2}\.\d{4}""".toRegex()
         if (!dateString.matches(regex)) {
-            throw IllegalArgumentException("Invalid date format. Expected format: dd.MM.yyyy")
+            throw IllegalArgumentException()
         }
     }
 }
